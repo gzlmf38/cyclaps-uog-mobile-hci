b827bd92ebec94ebe0d31ecc9e0ae00228955333
Milan Maximilian Mihaldinecz <mihaldinecz@outlook.com>
2019-03-02 18:08:36 +0000

    First bicycle race screen added ("select race"), using webview.

--- app/src/main/java/org/cyclaps/cyclaps/raceScreen.java
+++ app/src/main/java/org/cyclaps/cyclaps/raceScreen.java
@@ -1,15 +1,37 @@
 package org.cyclaps.cyclaps;
 
+import android.support.design.internal.BottomNavigationMenu;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebView;
 
 public class raceScreen extends AppCompatActivity {
 
+
+    private WebView wv;
+    private BottomNavigationMenu bnm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_race_screen);
 
 
+        BottomNavigationView bottomNavigationView;
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        //bottomNavigationView.setOnNavigationItemSelectedListener(myNavigationItemListener);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_race);
+
+
+        getSupportActionBar().hide();
+
+
+        wv = (WebView) findViewById(R.id.mwebView);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("http://mobilehci.s3-website.eu-west-2.amazonaws.com/race.html");
     }
+
+
+
 }
