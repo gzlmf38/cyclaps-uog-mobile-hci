b7d49502af08cfbdd012981fbafd87310abc83a8
Leo <gzlmf38@gmail.com>
2019-03-12 15:11:47 +0000

    update

--- app/src/main/java/org/cyclaps/cyclaps/homeScreen.java
+++ app/src/main/java/org/cyclaps/cyclaps/homeScreen.java
@@ -1,11 +1,15 @@
 package org.cyclaps.cyclaps;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class homeScreen extends AppCompatActivity {
@@ -13,6 +17,12 @@ public class homeScreen extends AppCompatActivity {
     private BottomNavigationView navigation = null;
 
     private TextView mTextMessage;
+
+    private Button bt_setting;
+
+    private Button btn_history;
+
+    private Button btn_collection;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -34,22 +44,49 @@ public class homeScreen extends AppCompatActivity {
                     return true;
                 case R.id.navigation_leaderboard:
                     mTextMessage.setText("Leaderboard");
+                    navigation.getMenu().getItem(3).setChecked(true);
+                    startActivity(new Intent(homeScreen.this, leaderboardScreen.class));
                     return true;
+
             }
             return false;
         }
     };
 
+    private class myOnClickListener implements View.OnClickListener{
+
+        private String flag = null;
+
+        private myOnClickListener(String name){
+            this.flag = name;
+        }
+        @Override
+        public void onClick(View v) {
+            if(this.flag=="setting"){
+                startActivity(new Intent(homeScreen.this, setting.class));
+            }
+            else if(this.flag=="history"){
+                startActivity(new Intent(homeScreen.this, history.class));
+            }
+            else if(this.flag=="collection"){
+                startActivity(new Intent(homeScreen.this, collection.class));
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-
+        getSupportActionBar().hide();
         mTextMessage = (TextView) findViewById(R.id.message);
         navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-
+        bt_setting = (Button) findViewById(R.id.btn_setting);
+        bt_setting.setOnClickListener(new myOnClickListener("setting"));
+        btn_history = (Button) findViewById(R.id.btn_history);
+        btn_history.setOnClickListener(new myOnClickListener("history"));
+        btn_collection = (Button) findViewById(R.id.btn_collection);
+        btn_collection.setOnClickListener(new myOnClickListener("collection"));
     }
-
 }
--- app/src/main/java/org/cyclaps/cyclaps/leaderboardScreen.java
+++ app/src/main/java/org/cyclaps/cyclaps/leaderboardScreen.java
@@ -1,9 +1,12 @@
 package org.cyclaps.cyclaps;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationMenu;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -13,16 +16,20 @@ public class leaderboardScreen extends AppCompatActivity {
 
     private WebView wv;
     private BottomNavigationMenu bnm;
+    private BottomNavigationView bottomNavigationView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard_screen);
 
 
-        BottomNavigationView bottomNavigationView;
+        //BottomNavigationView bottomNavigationView;
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         //bottomNavigationView.setOnNavigationItemSelectedListener(myNavigationItemListener);
         bottomNavigationView.setSelectedItemId(R.id.navigation_leaderboard);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
 
         getSupportActionBar().hide();
@@ -33,5 +40,35 @@ public class leaderboardScreen extends AppCompatActivity {
         wv.setWebViewClient(new WebViewClient());
         wv.loadUrl("http://mobilehci.s3-website.eu-west-2.amazonaws.com/leaderboard.html");
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    bottomNavigationView.getMenu().getItem(0).setChecked(true);
+                    startActivity(new Intent(leaderboardScreen.this, homeScreen.class));
+                    //mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_race:
+                    //mTextMessage.setText("Race");
+                    bottomNavigationView.getMenu().getItem(1).setChecked(true);
+                    startActivity(new Intent(leaderboardScreen.this, raceScreen.class));
+                    return true;
+                case R.id.navigation_hunt:
+                    // mTextMessage.setText("Hunt");
+                    return true;
+                case R.id.navigation_leaderboard:
+                    // mTextMessage.setText("Leaderboard");
+                    bottomNavigationView.getMenu().getItem(3).setChecked(true);
+                    startActivity(new Intent(leaderboardScreen.this, leaderboardScreen.class));
+                    return true;
+
+            }
+            return false;
+        }
+    };
 
 }
